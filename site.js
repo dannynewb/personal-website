5db0eb231198133ce19b65995769cf8e205862cc
dannynewb <dannynhull@hotmail.co.uk>
2020-05-11 10:48:59 +0100

    sorted burger menu

--- site.js
+++ site.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
 function changeBodyBg(className) {
     var heroBody = document.getElementsByClassName('hero-body');
     removeClasses(heroBody[0])
@@ -7,7 +33,7 @@ function changeBodyBg(className) {
 
     for (var navBarItemsIndex = 0; navBarItemsIndex < navbarItems.length; navBarItemsIndex++) {
         var currentNavbarItem = navbarItems[navBarItemsIndex];
-        
+
         removeClasses(currentNavbarItem)
 
         if (currentNavbarItem.classList.contains('is-active') || currentNavbarItem.id == 'colour-picker') {
