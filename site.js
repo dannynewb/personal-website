76b1fbb585b1dceb0349d87e204f6e7a35508b86
dannynewb <dannynhull@hotmail.co.uk>
2020-05-11 12:34:01 +0100

    keep selected theme in application storage

--- site.js
+++ site.js
@@ -1,30 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+    addBurgerClickEvent();
+    
+    var theme = localStorage.getItem('theme');
 
+    if (theme != 'null') {
+        changeBodyBg(theme);
+    }
+    else {
+        changeBodyBg('blue-jeans');
+    }
+});
+
+function setSelectedTheme() {
+
+}
+
+function addBurgerClickEvent() {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
+
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
         });
-      });
     }
-  
-  });
+}
 
 function changeBodyBg(className) {
+    localStorage.setItem('theme', className);
+
     var heroBody = document.getElementsByClassName('hero-body');
     removeClasses(heroBody[0])
     heroBody[0].classList.add(className);
